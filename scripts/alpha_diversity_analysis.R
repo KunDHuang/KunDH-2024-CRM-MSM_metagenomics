32bc14d440a71533a00bbeddc1b82ad022f8a920
KunDHuang <khuang1116@gmail.com>
2023-10-17 17:40:15 +0800

    created a function for making boxplot

--- scripts/alpha_diversity_analysis.R
+++ scripts/alpha_diversity_analysis.R
@@ -1,10 +1,14 @@
 
 
 ###### Testing code  ######
-#mpa_df <- data.frame(read.csv("/Users/kunhuang/R_analysis_mirror/msm_analysis/manuscript_prep/alpha_diversity_msm_nomsm_reproduce/merged_abundance_table_species_sgb_md.tsv",
-                    #  header = TRUE,
-                    #  sep = "\t"))
-#source(file = "/Users/kunhuang/repos/KunDH-2023-CRM-MSM_metagenomics/scripts/functions/alpha_diversity_funcs.R")
-#SE <- SE_converter(1:5, 6, mpa_df)
-###### Testing code  ######
+# mpa_df <- data.frame(read.csv("/Users/kunhuang/R_analysis_mirror/msm_analysis/manuscript_prep/alpha_diversity_msm_nomsm_reproduce/merged_abundance_table_species_sgb_md.tsv",
+#                      header = TRUE,
+#                      sep = "\t"))
+# source(file = "/Users/kunhuang/repos/KunDH-2023-CRM-MSM_metagenomics/scripts/functions/alpha_diversity_funcs.R")
+# SE <- SE_converter(1:5, 6, mpa_df)
 
+# alpha_df <- est_alpha_diversity(SE)
+
+# View(alpha_df)
+# make_boxplot(alpha_df, "sexual_orientation", "shannon", stats = FALSE, pal = c("#888888", "#eb2525"))
+##### Testing code  ######
--- scripts/functions/alpha_diversity_funcs.R
+++ scripts/functions/alpha_diversity_funcs.R
@@ -31,3 +31,54 @@ SE_converter <- function(md_rows, tax_starting_row, mpa_md) {
 
     SE_data
 }
+
+est_alpha_diversity <- function(se_data) {
+    # This function is to estimate alpha diversity (shannon index and richness) of a microbiome and output results in dataframe.
+    # se_data: the SummarizedExperiment data structure containing metadata and abundance values.
+    se_data <- se_data |>
+        mia::estimateRichness(abund_values = "relative_abundance", index = "observed")
+    se_data <- se_data |>
+        mia::estimateDiversity(abund_values = "relative_abundance", index = "shannon")
+    se_alpha_div <- data.frame(SummarizedExperiment::colData(se_data))
+    se_alpha_div
+}
+
+make_boxplot <- function(df, xlabel, ylabel, font_size = 11, jitter_width = 0.2, dot_size = 1, font_style = "Arial", stats = TRUE, pal = NULL) {
+    # This function is to create a boxplot using categorical data.
+    # df: The dataframe containing microbiome alpha diversities, e.g. `shannon` and `observed` with categorical metadata.
+    # xlabel: the column name one will put along x-axis.
+    # ylabel: the index estimate one will put along y-axis.
+    # font_size: the font size, default: [11]
+    # jitter_width: the jitter width, default: [0.2]
+    # dot_size: the dot size inside the boxplot, default: [1]
+    # font_style: the font style, default: `Arial`
+    # pal: a list of color codes for pallete, e.g. c(#888888, #eb2525). The order corresponds the column order of boxplot.
+    # stats: wilcox rank-sum test. default: TRUE
+    if (stats) {
+          nr_group = length(unique(df[, xlabel])) # get the number of groups
+          if (nr_group == 2) {
+              group_pair = list(unique(df[, xlabel]))
+              ggpubr::ggboxplot(data = df, x = xlabel, y = ylabel, color = xlabel,
+              palette = pal, ylab = ylabel, xlab = xlabel,
+              add = "jitter", add.params = list(size = dot_size, jitter = jitter_width)) +
+              ggpubr::stat_compare_means(comparisons = group_pair, exact = T, alternative = "less") +
+              ggplot2::stat_summary(fun.data = function(x) data.frame(y = max(df[, ylabel]), label = paste("Mean=",mean(x))), geom="text") +
+              ggplot2::theme(text = ggplot2::element_text(size = font_size, family = font_style))
+           }
+          else {
+              group_pairs = my_combn(unique((df[, xlabel])))
+              ggpubr::ggboxplot(data = df, x = xlabel, y = ylabel, color = xlabel,
+              palette = pal, ylab = ylabel, xlab = xlabel,
+              add = "jitter", add.params = list(size = dot_size, jitter = jitter_width)) +
+              ggpubr::stat_compare_means() + stat_compare_means(comparisons = group_pairs, exact = T, alternative = "greater") +
+              ggplot2::stat_summary(fun.data = function(x) data.frame(y= max(df[, ylabel]), label = paste("Mean=",mean(x))), geom="text") +
+              ggplot2::theme(text = ggplot2::element_text(size = font_size, family = font_style))
+           }
+    }
+    else {
+        ggpubr::ggboxplot(data = df, x = xlabel, y = ylabel, color = xlabel,
+        palette = pal, ylab = ylabel, xlab = xlabel,
+        add = "jitter", add.params = list(size = dot_size, jitter = jitter_width)) +
+        ggplot2::theme(text = ggplot2::element_text(size = font_size, family = font_style))
+    }
+}
